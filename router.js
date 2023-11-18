417e7da496074f52e4d18b20c687bd12a17b48dc
D_Cervantes <116675748+AveryWorks@users.noreply.github.com>
2023-11-18 01:44:13 -0600

    Se hizo lo que falta, que es, no c

--- router.js
+++ router.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const conexion = require('./database/db');
 const controller = require('./controller/controller');
+const res = require('express/lib/response');
+const req = require('express/lib/request');
 
 //Inicio de usuario
 
@@ -194,22 +196,224 @@ router.get('/BloqueadoView', (req,res)=>{
     res.render('BloqueadoView');
     
 });
-router.get('/UsuarioView', (req,res)=>{
+router.get('/UsuarioView/:id', (req,res)=>{
+    const id = req.params.id;
+  console.log("Id encontrada: "+id);
+  const usuario = "SELECT * FROM usuario WHERE ID=?" ;
+  const buscarmonedero = "SELECT * FROM monedero WHERE usuarioref = ?";
+  const obtenerHistorial = "SELECT * FROM transacciones WHERE idUsuario = ?";
 
-    res.render('UsuarioView');
+  conexion.query(usuario, [id], (err, infousuario) => {
+        if (err) {
+
+            throw err;
+
+        } else {
+
+            conexion.query(buscarmonedero,[infousuario[0].cedulaUsuario],(err, monedero) => {
+
+                if (err) {
+                  console.error(err);
+                } else {
+                  console.log("Monedero encontrado: ", monedero[0]);
+                  conexion.query(obtenerHistorial,[infousuario[0].cedulaUsuario],(err, historial) => {
+                      if (err) {
+        
+                        throw err;
+        
+                      } else {
+        
+                        console.log("Historial: " + historial[0]);
+                        res.render("UsuarioView", {usuario: infousuario[0],monedero: monedero[0],transaccion: historial[0]});
+        
+                      }
+                    }
+                  );
+                }
+            });
+        }
+    })
+
+
+    
     
 });
-router.get('/ChoferView', (req,res)=>{
 
-    res.render('ChoferView');
+//Recargar saldo
+
+
+router.get('/recargar/:cedula/:monto/:id', (req, res) => {
+    const id = req.params.id;
+    const cedula = req.params.cedula;
+    const monto = req.params.monto;
+    var nuevoSaldo = 0;
+    const usuarioQuery = "SELECT * FROM usuario WHERE cedulaUsuario=?";
+    const buscarMonederoQuery = "SELECT * FROM monedero WHERE usuarioref = ?";
+    const actualizarSaldoQuery = "UPDATE monedero SET saldo = ? WHERE usuarioref = ?";
+
+    conexion.query(usuarioQuery, [cedula], (err, infousuario) => {
+        if (err) {
+            throw err;
+        } else {
+            try {
+                if (infousuario.length > 0) {
+                    console.log("Usuario encontrado:", infousuario[0]);
+
+                    conexion.query(buscarMonederoQuery, [infousuario[0].cedulaUsuario], (err, monedero) => {
+                        if (err) {
+                            throw err;
+                        } else {
+                            // Verificar si se encontró un monedero asociado al usuario
+                            if (monedero.length > 0) {
+                                // Realizar la actualización del saldo
+                                
+                                nuevoSaldo = monedero[0].saldo + parseInt(monto);
+
+                                conexion.query(actualizarSaldoQuery, [nuevoSaldo, infousuario[0].cedulaUsuario], (err, resultado) => {
+                                    if (err) {
+                                        throw err;
+                                    } else {
+                                        console.log("Saldo actualizado correctamente");
+                                        // Puedes realizar otras acciones o enviar una respuesta al cliente aquí
+                                    }
+                                });
+                            } else {
+                                // No se encontró un monedero asociado al usuario
+                                console.log("No se encontró un monedero asociado al usuario");
+                                // Puedes realizar otras acciones o enviar una respuesta al cliente aquí
+                            }
+                        }
+                    });
+                } else {
+                    console.log("Usuario no encontrado");
+                    res.send("Usuario no encontrado en la base de datos");
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    });
+    res.redirect('/UsuarioView/' + id);
+});
+
+
+//Comprobar cobrar
+router.get('/cobrar/:cedula/:monto/:id', (req,res)=>{
+    const id = req.params.id;
+    const cedula = req.params.cedula;
+    const monto = req.params.monto;
+
+    const usuario = "SELECT * FROM usuario WHERE cedulaUsuario=?" ;
+    const buscarmonedero = "SELECT * FROM monedero WHERE usuarioref = ?";
+
+    conexion.query(usuario, [cedula], (err, infousuario) => {
+
+        if (err) {
+            throw err
+        } else {
+            try {
+                if (infousuario.length > 0) {
+                
+                    console.log("Usuario encontrado:", infousuario[0]);
+                    console.log("Monto a cobrar:", monto);
+                    conexion.query(buscarmonedero, [infousuario[0].cedulaUsuario], (err, monedero) => {
+                        if (monedero[0].saldo >= monto) {
+                            // Realizar la actualización del saldo
+                            const nuevoSaldo = monedero[0].saldo - monto;
+                            const actualizarSaldo = "UPDATE monedero SET saldo = ? WHERE usuarioref = ?";
+
+                            conexion.query(actualizarSaldo, [nuevoSaldo, infousuario[0].cedulaUsuario], (err, resultado) => {
+                                if (err) {
+                                    throw err;
+                                } else {
+                                    console.log("Saldo actualizado correctamente");
+                                    // Puedes realizar otras acciones o enviar una respuesta al cliente aquí
+                                }
+                            });
+                        } else {
+                            // El saldo no es suficiente
+                            console.log("Saldo insuficiente");
+                            // Puedes realizar otras acciones o enviar una respuesta al cliente aquí
+                        }
+
+                    })
+
+                } else {
+                    console.log("Usuario no encontrado");
+                    res.send("Usuario no encontrado en la base de datos");
+                    
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    });
+    res.redirect('/Choferview/'+id)
+
+})
+
+//Cargar plantilla chofer segun ID
+router.get("/ChoferView/:id", (req, res) => {
     
+  const id = req.params.id;
+  console.log("Id encontrada: "+id);
+  const usuario = "SELECT * FROM usuario WHERE ID=?" ;
+  const buscarmonedero = "SELECT * FROM monedero WHERE usuarioref = ?";
+  const obtenerHistorial = "SELECT * FROM transacciones WHERE idUsuario = ?";
+  const rutaus =
+    "SELECT r.* FROM ruta_usuario ru JOIN ruta r ON ru.idRuta = r.idRuta WHERE ru.cedulaUsuario = ? ;";
+
+  conexion.query(usuario, [id], (err, infousuario) => {
+    if (err) {
+      throw err;
+    } else {
+        console.log(infousuario[0]);
+        conexion.query(buscarmonedero, [infousuario[0].cedulaUsuario], (err, monedero) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log("Monedero encontrado: ", monedero[0]);
+              conexion.query(
+                obtenerHistorial,
+                [infousuario[0].cedulaUsuario],
+                (err, historial) => {
+                  if (err) {
+                    throw err;
+                  } else {
+                    console.log("Historial: " + historial[0]);
+        
+                    conexion.query(
+                      rutaus,
+                      [infousuario[0].cedulaUsuario],
+                      (err, rutausuario) => {
+                        if (err) {
+                          throw err;
+                        } else {
+                          console.log("Rutas y usuario: " + rutausuario[0]);
+                          res.render("ChoferView", {
+                            usuario: infousuario[0],
+                            monedero: monedero[0],
+                            transaccion: historial[0],
+                            rutausuario: rutausuario[0],
+                          });
+                        }
+                      }
+                    );
+                  }
+                }
+              );
+            }
+          });
+    }
+  });
+  
 });
 router.post("/loguearse", controller.loguearse);
-router.post('/saveUS',controller.saveUS);
-router.post('/saveRutaUsuario',controller.saveRutaUsuario);
-router.post('/saveReg',controller.saveReg);
-router.post('/saveRuta',controller.saveRuta);
-router.post('/updateUS',controller.updateUS);
-router.post('/updateRuta',controller.updateRuta);
+router.post("/saveUS", controller.saveUS);
+router.post("/saveRutaUsuario", controller.saveRutaUsuario);
+router.post("/saveReg", controller.saveReg);
+router.post("/saveRuta", controller.saveRuta);
+router.post("/updateUS", controller.updateUS);
+router.post("/updateRuta", controller.updateRuta);
 
-module.exports=router;
+module.exports = router;
